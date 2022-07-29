59623f3d6db52da7030b1c21b18aee27b155975d
Remix Workspace <>
2022-07-29 18:10:55 -0300

    Security in safeMint function

--- contracts/Loterie.sol
+++ contracts/Loterie.sol
@@ -43,9 +43,54 @@ contract Loterie is ERC20, Ownable {
         _mint(address(this), _amount);
     }
 
+    // Users Registrer
+    function registrar() internal {
+        address personal_contract_address = address(new ticketsNFTs(msg.sender, address(this), nft));
+        user_contract[msg.sender] = personal_contract_address;
+    }
+
+    // Info from an User
+    function userInfo(address _account) public view returns (address) {
+        return user_contract[_account];
+    }
+
 }
 
 contract mainERC721 is ERC721 {
 
-    constructor() ERC721("Loterie", "LOT Ticket") {}
+    address public loterieAddress;
+
+    constructor() ERC721("Loterie", "LOT Ticket") {
+        // This contract is deployed automatically by the Loterie contract
+        loterieAddress = msg.sender;
+    }
+
+    // NFT Creation
+    function safeMint(address _owner, uint256 _ticket) public {
+        require(msg.sender == Loterie(loterieAddress).userInfo(_owner), "You don't have access to execute this function");
+        _safeMint(_owner, _ticket);
+    }
+
+}
+
+contract ticketsNFTs {
+
+    struct Owner {
+        address ownerAddress;
+        address loterieAddress;
+        address nftContract;
+        address userContract;
+    }
+
+    Owner public owner;
+
+    constructor(address _owner, address _loterieAddress, address _nftContract) {
+        owner = Owner(_owner, _loterieAddress, _nftContract, address(this));
+    }
+
+    function mintTicket(address _owner, uint _ticket) public {
+        require(msg.sender == owner.loterieAddress, "You don't have access to execute this function");
+        mainERC721(owner.nftContract).safeMint(_owner, _ticket);
+    }
+
 }
